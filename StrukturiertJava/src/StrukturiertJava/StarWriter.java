ce6d3d79dba7a9e9a08c767e4e12cd673f35216a
altin <kelmendialtin48@gmail.com>
2022-08-28 20:22:37 +0200

    add StarWriter

--- StrukturiertJava/src/StrukturiertJava/StarWriter.java
+++ StrukturiertJava/src/StrukturiertJava/StarWriter.java
@@ -1,4 +1,29 @@
 package StrukturiertJava;
 
+import java.util.Scanner;
+
 public class StarWriter {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt(); // Eingabezahl
+        System.out.print(": ");
+        while(x%2==0 || x<1 ){ //Ungültige Zahlen Filtern
+            System.out.print(": ");
+            x=sc.nextInt();
+        }
+        int fix=x; //Sterne von Links
+        int m=x;   // Sterne von Rechts
+        for(int n =x;n>0;n--){ // vertikale Achse
+
+            for(int i=x;i>0;i--){//horizontale Achse
+                System.out.print((fix-i==0)||(m-fix==i-1)||i-1==m/2||n-1==m/2?"*":"-");
+
+            }
+            System.out.print("\n");// Zeilenumbruch nach jeder x
+            fix--;
+        }
+
+        sc.close();
+    }
 }
