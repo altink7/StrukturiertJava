5cb47215afe1b80975b01ea80222e57c751be719
altink7 <kelmendialtin48@gmail.com>
2023-10-18 20:58:04 +0200

    warnings fixed for diagonale

--- StrukturiertJava/src/StrukturiertJava/Apps/Diagonale.java
+++ StrukturiertJava/src/StrukturiertJava/Apps/Diagonale.java
@@ -6,9 +6,9 @@ public class Diagonale {
 
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt(); // Eingabezahl
-        System.out.printf(": ");
+        System.out.print(": ");
         while(x%2==0 || x<1 ){ //Ungültige Zahlen Filtern
-            System.out.printf(": ");
+            System.out.print(": ");
             x=sc.nextInt();
         }
         int fix=x; //Sterne von Links
@@ -19,11 +19,11 @@ public class Diagonale {
                 System.out.print((fix-i==0)||(m-fix==i-1)?"*":"-");
 
             }
-            System.out.printf("\n");// Zeilenumbruch nach jeder x
+            System.out.print("\n");// Zeilenumbruch nach jeder x
             fix--;
         }
 
-        System.out.printf("");//nur zum Kopieren hier , keine Funktion daweil
+        System.out.print("");//nur zum Kopieren hier, keine Funktion da weil
         sc.close();
     }
 }
