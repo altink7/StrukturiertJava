21ebe49e6f9251acfb3588f63b61eec820599cae
altink7 <kelmendialtin48@gmail.com>
2023-10-18 20:57:20 +0200

    removed unnecessary code parts

--- StrukturiertJava/src/StrukturiertJava/Apps/CodematchStick.java
+++ StrukturiertJava/src/StrukturiertJava/Apps/CodematchStick.java
@@ -7,14 +7,10 @@ public class CodematchStick{
         Scanner sc = new Scanner(System.in);
 
         int sticks = 30;
-        int fixstick=sticks;
-        int pcsticks = 30;
-        int input=0;
+        int input;
 
         int sticks2 = 30;
-        int fixstick2=sticks;
-        int pcsticks2 = 30;
-        int input2=0;
+        int input2;
 
         System.out.println("Welcome to the matchstick game!");
         System.out.println("Please enter your name");
@@ -28,13 +24,12 @@ public class CodematchStick{
             System.out.printf("Player 1 Sticks left:%d\n", sticks);
             while (sticks>0) {
                 for (int i = 0; i < sticks; i++) {
-                    System.out.printf("|");
+                    System.out.print("|");
                     System.out.print((i % 5 == 4) ? "  " : "");
-                    fixstick--;
 
 
                 }
-                System.out.printf("\nPlayer 1 take 1-3 sticks:");
+                System.out.print("\nPlayer 1 take 1-3 sticks:");
                 input = sc.nextInt();
                 while (input < 1 || input > 3) {
                     System.out.println("Bitte noch einmal versuchen:");
@@ -51,19 +46,18 @@ public class CodematchStick{
 
         else if(comp.equals("y")){
 
-            while (sticks>0) {
+            while (true) {
 
                 for (int j=0;j<1;j++) {
 
                     System.out.printf("Player 1 Sticks left:%d\n", sticks);
                     for (int i = 0; i < sticks; i++) {
-                        System.out.printf("|");
+                        System.out.print("|");
                         System.out.print((i % 5 == 4) ? "  " : "");
-                        fixstick--;
 
 
                     }
-                    System.out.printf("\nPlayer 1 take 1-3 sticks:");
+                    System.out.print("\nPlayer 1 take 1-3 sticks:");
                     input = sc.nextInt();
                     while (input < 1 || input > 3) {
                         System.out.println("Bitte noch einmal versuchen:");
@@ -74,18 +68,17 @@ public class CodematchStick{
                         sticks = sticks - input;
                     }
                 }
-                System.out.printf("\n\n");
+                System.out.print("\n\n");
 
                 for (int k=0;k<1;k++) {
                     System.out.printf("Player 2 Sticks left:%d\n", sticks2);
                     for (int i1 = 0; i1 < sticks2; i1++) {
-                        System.out.printf("|");
+                        System.out.print("|");
                         System.out.print((i1 % 5 == 4) ? "  " : "");
-                        fixstick2--;
 
 
                     }
-                    System.out.printf("\nPlayer 1 take 1-3 sticks:");
+                    System.out.print("\nPlayer 1 take 1-3 sticks:");
                     input2 = sc.nextInt();
                     while (input2 < 1 || input2 > 3) {
                         System.out.println("Bitte noch einmal versuchen:");
@@ -97,7 +90,7 @@ public class CodematchStick{
                     }
 
                 }
-                System.out.printf("\n\n");
+                System.out.print("\n\n");
 
                 if (sticks2<1 && sticks >0){
                     System.out.println("Player 2 won!!");
@@ -107,16 +100,11 @@ public class CodematchStick{
                     System.out.println("Player 1 won!!");
                     break;
                 }
-                else if(sticks<1&& sticks2<1) {
+                else if(sticks<1) {
                     System.out.println("It is a TIEEEE!!!");
                     break;
                 }
-
-
             }
-
-
         }
-
     }
 }
